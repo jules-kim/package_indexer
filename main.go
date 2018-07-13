2d83bd2aa787c1db9dafd169b64075565cb1a6f3
Anonymous <>
2018-07-12 17:43:12 -0700

    handle connections from clients

--- main.go
+++ main.go
@@ -1,10 +1,5 @@
 package main
-	
-import "fmt"
-
 
 func main() {
 	StartServer()
-	// Test()
-	fmt.Println("hi")
 }
--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"log"
+	"bufio"
 )
 
 const (
@@ -12,23 +13,40 @@ const (
 )
 
 // start running the server with tcp connection 
+// for multiple client connections 
+// returns and logs any errors 
 func StartServer() error {
-	ln, err := net.Listen(CONN_TYPE, PORT)					/* set up server listening 	*/ 
+	ln, err := net.Listen(CONN_TYPE, PORT)				/* set up server listening 	*/ 
 	if err != nil {
 		log.Printf("%s", err)
 		return err
 	}
 	
 	fmt.Println("Server is running...")
-	fmt.Println("Server listening on port...", PORT)		/* successfully listening 	*/
+	fmt.Println("Server listening on port...", PORT)	/* successfully listening 	*/
 	
-	for {													/* accept incoming requests */ 
-		_, err := ln.Accept() // conn 
+	for {												/* accept incoming requests */ 
+		conn, err := ln.Accept()  
 		if err != nil {
 			log.Printf("%s", err)
 			return err 
 		}
-		fmt.Println("Request from client made.")
-		//go handleConnection(conn)							/* handle client requests 	*/
+		fmt.Println("Request from client made")
+		// go routine 
+		go handleConnection(conn)						/* handle client connection */
 	}
 }
+
+// handles client connections 
+// reads input from buffer
+func handleConnection(conn net.Conn) {
+	fmt.Println("Handling new connection...")
+	reader := bufio.NewReader(conn)						/* Set up buffer reader 	*/ 	
+	request, err := reader.ReadString('\n') 			/* Read client request 		*/ 
+	if err != nil {
+		log.Fatal("%s", err)
+	}
+	fmt.Println(request)
+}
+
+
