ad5eb3b6fa75196936c34482b6fb83b87ba76c36
Anonymous <>
2018-07-12 18:15:54 -0700

    changed return type of parser to Request

--- parse.go
+++ parse.go
@@ -9,6 +9,7 @@ import (
 // parseRequest takes the client request in the form of a
 // byte array and splits the request into its three parts
 // <command> <package> <dependencies> 
+// It returns these three in the Request struct 
 func ParseRequest(req []byte) Request {
 	
 
