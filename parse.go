8346fa7170de8c90a1056b6ce46c12972bf86005
Anonymous <>
2018-07-12 18:08:57 -0700

    updated parseRequest input type

--- parse.go
+++ parse.go
@@ -3,5 +3,13 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
+// parseRequest takes the client request in the form of a
+// byte array and splits the request into its three parts
+// <command> <package> <dependencies> 
+func ParseRequest(req []byte) Request {
+	
+
+}
