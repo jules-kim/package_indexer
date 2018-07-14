f87ccf495d1d513c03581c46f1b749a07ce4203f
Anonymous <>
2018-07-13 17:45:42 -0700

    new structs added to types file

--- types.go
+++ types.go
@@ -1,5 +1,7 @@
 package main 
 
+import "sync"
+
 // This type is used to encapsulate the 
 // three aspects of the client request,
 // the command, package, and dependencies 
@@ -8,4 +10,18 @@ type Request struct {
 	pack 	string 
 	dep 	[]string
 	err 	string 
+}
+
+// This type is used for package storage
+// within a map on the server 
+type PackageIndexer struct {
+	packs 	map[string]*Package 
+	mutex 	*sync.Mutex
+}
+
+// This type is used to represent packages
+// and their dependencies 
+type Package struct {
+	Name	string 
+	deps 	[]*Package
 }
