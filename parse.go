746e7e1fadecb01e415478b7f8b0c9f3ff9323e4
Anonymous <>
2018-07-14 17:57:08 -0700

    updates to parse and parse_test

--- parse.go
+++ parse.go
@@ -1,36 +1,31 @@
 package main 
 
-import (
-	"bytes"
-)
+import "strings"
 
 // file to parse client requests into command, 
 // package, and dependencies
 
 // parseRequest takes the client request in the form of a
-// byte array and splits the request into its three parts
+// string and splits the request into its three parts
 // <command> <package> <dependencies> 
 // It returns these three in a Request struct 
-func ParseRequest(req []byte) Request {
-	splitReq := bytes.Split(req, []byte("|"))					/* split the byte[] by "|"		*/
+func ParseRequest(req string) Request {
+	// TODO: trim spaces 
+	splitReq := strings.Split(req, "|")							/* split the string by "|"		*/
 	if len(splitReq) != 3 {
 		return Request{"", "", nil, "Incorrect request format"}
 	}
 	deps := parseDep(splitReq[2])								/* parse deps by "," 			*/
-	com := string(splitReq[0][:])								/* convert to a string 			*/ 
-	pack := string(splitReq[1][:])
+	com := splitReq[0]
+	pack := splitReq[1]
 	
 	return Request{com, pack, deps, ""}  
 }
 
 // parse dependencies by "," and returns 
-// the dependencies converted to an array of strings
-func parseDep(d []byte) []string {
-	deps_bytes := bytes.Split(d, []byte(","))					/* split deps[] by ","			*/ 
-	deps := make([]string, len(deps_bytes))
-	for i, dep := range deps_bytes {							/* convert each ele to a str 	*/ 
-		deps[i] = string(dep[:])
-	}
+// the dependencies as an array of strings 
+func parseDep(d string) []string {
+	deps := strings.Split(d, ",")								/* split deps[] by ","			*/ 
 
 	return deps
 }
--- parse_test.go
+++ parse_test.go
@@ -7,19 +7,19 @@ import (
 
 func TestParseRequest(t *testing.T) {
 	// One dependency included 
-	req := ParseRequest([]byte("INDEX|cloog|gmp"))
+	req := ParseRequest("INDEX|cloog|gmp")
     if req.comm != "INDEX" || req.pack != "cloog" || 
     !reflect.DeepEqual([]string{"gmp"}, req.dep) {
        t.Errorf("Parsing request with one dependency was incorrect") 
     }
     // No dependencies included 
-    req2 := ParseRequest([]byte("INDEX|cloog|"))
+    req2 := ParseRequest("INDEX|cloog|")
     if req2.comm != "INDEX" || req2.pack != "cloog" ||
     !reflect.DeepEqual([]string{""}, req2.dep) {
        t.Errorf("Parsing request with no dependencies was incorrect") 
     }
     // Multiple dependencies included 
-    req3 := ParseRequest([]byte("INDEX|cloog|gmp,isl,pkg-config"))
+    req3 := ParseRequest("INDEX|cloog|gmp,isl,pkg-config")
     arr := []string{"gmp","isl","pkg-config"}
     if req3.comm != "INDEX" || req3.pack != "cloog" || 
     !reflect.DeepEqual(arr, req3.dep) {
@@ -27,19 +27,19 @@ func TestParseRequest(t *testing.T) {
     }
     // Testing badly formatted requests 
     // Testing too many | characters 
-    req4 := ParseRequest([]byte("INDEX|cloog|gmp|"))
+    req4 := ParseRequest("INDEX|cloog|gmp|")
     if req4.err == "" {
     	t.Errorf("Incorrect catching of formatting error")
     }
     // Testing incorrect message pattern 
-    req5 := ParseRequest([]byte("INDEX|"))
+    req5 := ParseRequest("INDEX|")
     if req5.err == "" {
     	t.Errorf("Incorrect catching of formatting error")
     }
 }
 
 func TestParseDep(t *testing.T) {
-	deps := parseDep([]byte("gmp,isl,pkg-config"))
+	deps := parseDep("gmp,isl,pkg-config")
 	arr := []string{"gmp","isl","pkg-config"}
 	if !reflect.DeepEqual(arr, deps) {
 		t.Errorf("Parsing of dependencies failed") 
